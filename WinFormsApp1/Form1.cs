1bb01e1b42d053b364cffd2d63c1483b701d908c
Alex <Alex@DESKTOP-QAV09J1>
2024-05-27 11:25:02 +0300

    1

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,10 +1,42 @@
-namespace WinFormsApp1
+ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Thread th;
+        Thread th1;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            th = new Thread(thread);
+            th.Start();
+        }
+        public void thread() 
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(50);
+            }
+            MessageBox.Show("completed blue");
+        }
+
+        public void threadb()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(50);
+            }
+            MessageBox.Show("completed red");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            th1 = new Thread(threadb);
+            th1.Start();
         }
     }
 }
